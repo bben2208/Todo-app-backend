873ebbdd3b2a5cb5ace3b4085eedb214020d390b
Benjamin Bognar <benjaminbognar@MacBook-Pro-2.local>
2025-03-08 12:48:22 +0000

    data

--- config/database.js
+++ config/database.js
@@ -1,20 +1,18 @@
-// config/database.js
 const mongoose = require('mongoose');
 
-console.log("MONGO_URL:", process.env.MONGO_URL); // Debug: Check that it's loaded
+console.log("MONGO_URL:", process.env.MONGO_URL); // Debug: should print your connection string
 
 const uri = process.env.MONGO_URL;
-
 if (!uri) {
   console.error("Missing MONGO_URL environment variable");
-  process.exit(1);
+  process.exit(1); // Exit if it's missing
 }
 
 mongoose.connect(uri, { 
   useNewUrlParser: true, 
   useUnifiedTopology: true 
 })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.error("MongoDB connection error:", err));
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 module.exports = mongoose.connection;
